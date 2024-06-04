04be69314b772dcdd369b0a6d60b24274533fd28
sr-roboto <marcoslopez123.lu@gmail.com>
2024-06-04 15:57:23 -0300

    cree un innerHTML en el cual llamo al genero, nombre, especie, ubicacion de cada personaje

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # Fetch-MarcosLopez
 # Actividad: consumiendo y vizualizando datos de una API pública
+# La api que utilice fue la misma utilizada por los profesores que usaron a modo de ejemplo
--- assets/JS/main.js
+++ assets/JS/main.js
@@ -1,5 +1,3 @@
-
-
 const elContenido = document.getElementById('container')
 const url = "https://rickandmortyapi.com/api/character"
 
@@ -12,30 +10,19 @@ const proceso = async (link) => {
         console.log("Hubo un error")
     }
 }
-
-// const elemento = async () => {
-//     try {
-//         const respuesta = await fetch(url)
-//         const data = await respuesta.json()
-//         const info = data.results
-
-//         info.forEach(inf => {
-//             const item = document.createElement("div")
-//             item.className = "nombre"
-//             item.innerHTML += `
-//             <h1>${inf.name}</h1>`;
-//             elcontenido.appendChild(item);
-//         })
-//     } catch (error) {
-//         console.error(error)
-//     }
-// }
-
 proceso(url)
-    .then ((info) => {
+    .then((info) => {
         info.results.forEach((elemento) => {
-            elContenido.innerHTML += `
-            <h3>${elemento.name}</h3>
+            const items = document.createElement("div")
+            items.className = "personaje m-3 border rounded border-4 border-dark bg-dark shadow-lg"
+            items.innerHTML += `
+            <img src="${elemento.image}" alt="${elemento.name}" class="rounded">
+            <h3 class="text-white fs-4 text-center">${elemento.name}</h3>
+            <p class="text-white">Especie: ${elemento.species}</p>
+            <p class="text-white">Sexo: ${elemento.gender}</p>
+            <p class="text-white">Origen: ${elemento.origin.name}</p>
+            <p class="text-white">Locación: ${elemento.location.name}</p>
             `
+            elContenido.appendChild(items)
         })
     })
